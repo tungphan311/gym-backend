d6330fcbd70e008b009d77cd25f6f801c233c153
tungphan311 <thanhtunga1lqd@gmail.com>
2019-12-18 00:10:54 +0700

    update accounts

--- db/init.go
+++ db/init.go
@@ -15,16 +15,34 @@ const (
 	dbname   = "gymdb"
 )
 
-var (
-	types = [2]StaffType{StaffType{Name: "fulltime"}, StaffType{Name: "partime"}}
-	roles = [5]Role{Role{Name: "admin"}, Role{Name: "trainer"}, Role{Name: "receptionist"}, Role{Name: "accountant"}, Role{Name: "equipment manager"}}
-)
-
 func init() {
 	db := Connect()
 
 	db.AutoMigrate(&Staff{}, &StaffType{}, &Role{}, &Account{})
 
+	initData(db)
+}
+
+func Connect() *gorm.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+var (
+	types    = [2]StaffType{StaffType{Name: "fulltime"}, StaffType{Name: "partime"}}
+	roles    = [5]Role{Role{Name: "admin"}, Role{Name: "trainer"}, Role{Name: "receptionist"}, Role{Name: "accountant"}, Role{Name: "equipment manager"}}
+	accounts = [1]Account{Account{StaffID: 1, Username: "tungpt@gmail.com", Password: "password"}}
+)
+
+func initData(db *gorm.DB) {
 	var staffTypes StaffType
 	count := 0
 	db.Find(&staffTypes).Count(&count)
@@ -45,17 +63,14 @@ func init() {
 			db.Create(&newRole)
 		}
 	}
-}
 
-func Connect() *gorm.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	var account Account
+	db.Find(&account).Count(&count)
 
-	db, err := gorm.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
+	if count == 0 {
+		for i := 0; i < len(accounts); i++ {
+			newAccount := accounts[i]
+			db.Create(&newAccount)
+		}
 	}
-
-	return db
 }
